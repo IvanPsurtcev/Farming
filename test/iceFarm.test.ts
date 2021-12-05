77728fffb2c2a8131a9fb909b5c270ec6208c9bc
IvanPsurtcev <87025698+IvanPsurtcev@users.noreply.github.com>
2021-12-05 16:17:04 +0300

    update

--- test/iceFarm.test.ts
+++ test/iceFarm.test.ts
@@ -59,5 +59,58 @@ describe("Init", async() => {
         expect(iceToken).to.be.ok;
         expect(iceFarm).to.be.ok;
         expect(mockDai).to.be.ok;
-    })
-})
+    });
+});
+
+describe("Stake", async() => {
+    it("should accept DAI and update mapping", async() => {
+        let toTransfer = ethers.utils.parseEther("100")
+        await mockDai.connect(alice).approve(iceFarm.address, toTransfer)
+
+        expect(await iceFarm.isStaking(alice.address)).to.eq(false)
+
+        expect(await iceFarm.connect(alice).stake(toTransfer)).to.be.ok
+
+        expect(await iceFarm.stakingBalance(alice.address)).to.eq(toTransfer)
+
+        expect(await iceFarm.isStaking(alice.address)).to.eq(true)
+    });
+
+    it("should update balance with multiple stakes", async() => {
+        let toTransfer = ethers.utils.parseEther("100")
+        await mockDai.connect(alice).approve(iceFarm.address, toTransfer)
+        await iceFarm.connect(alice).stake(toTransfer)
+
+        await mockDai.connect(alice).approve(iceFarm.address, toTransfer)
+        await iceFarm.connect(alice).stake(toTransfer)
+
+        expect(await iceFarm.stakingBalance(alice.address)).to.eq(ethers.utils.parseEther("200"))
+    });
+
+    it("should revert with not enough funds", async() => {
+        let toTransfer = ethers.utils.parseEther("1000000")
+        await mockDai.approve(iceFarm.address, toTransfer)
+
+        await expect(iceFarm.connect(bob).stake(toTransfer)).to.be.revertedWith("You cannot stake zero tokens")
+    });
+});
+
+describe("Unsake", async() => {
+    beforeEach(async() => {
+        let ToTransfer = ethers.utils.parseEther("100")
+        await mockDai.connect(alice).approve(iceFarm.address, toTransfer)
+        await iceFarm.connect(alice).stake(toTransfer)
+    });
+
+    it("should unstake balance from user", async() => {
+        let toTransfer = ethers.utils.parseEther("100")
+        await iceFarm.connect(alice).unstake(toTransfer)
+
+        res = await iceFarm.stakingBalance(alice.address)
+        expect(Number(res))
+            .to.eq(0)
+
+        expect(await iceFarm.isStaking(alice.address))
+            .to.eq(false)
+    });
+});
